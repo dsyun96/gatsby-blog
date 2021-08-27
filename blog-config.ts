218e7e422c8311b5721c294043bc29c9b1b735bb
Changhui Lee <iam@xo.dev>
2021-08-27 23:09:23 +0900

    feat: add new configuration for default blog thumbnail

--- blog-config.ts
+++ blog-config.ts
@@ -3,6 +3,7 @@ const config = {
   author: 'Daeseung Yun',
   description: '모르는 것이 많지만 하나하나 새롭게 알아가는 것이 즐거운 개발자입니다. 제일 잘 하는 것은 삽질이며, 제가 했던 삽질을 다른 사람이 반복하지 않았으면 좋겠다는 생각에 삽질의 내용을 이곳에 기록하고자 합니다.',
   siteUrl: 'https://yuni.vercel.app',
+  thumbnail: '/thumbnails/main.png',
   social: {
     github: 'dsyun96',  // GitHub username
     twitter: '',        // Twitter username
